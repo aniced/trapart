7f99d3e0808877f6f06f34d55e19ec025e92c85c
Frog Chen <satgo1546@yeah.net>
2022-10-06 18:08:04 +0800

    Remove components Icon*

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
-      <DocumentationIcon />
     </template>
     <template #heading>Documentation</template>
 
@@ -21,7 +15,6 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
     </template>
     <template #heading>Tooling</template>
 
@@ -42,7 +35,6 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <EcosystemIcon />
     </template>
     <template #heading>Ecosystem</template>
 
@@ -58,7 +50,6 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <CommunityIcon />
     </template>
     <template #heading>Community</template>
 
@@ -73,7 +64,6 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <SupportIcon />
     </template>
     <template #heading>Support Vue</template>
 
