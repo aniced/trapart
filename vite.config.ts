15362550ed686fa8922f8c616eb8e9eb80d24314
Frog Chen <satgo1546@yeah.net>
2023-09-30 07:56:51 +0800

    Add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    script: {
-      defineModel: true,
-    },
-  })],
+  plugins: [
+    vue({
+      script: {
+        defineModel: true,
+      },
+    }),
+    visualizer({
+      emitFile: true,
+      filename: 'bundle.html',
+    }),
+  ],
 })
