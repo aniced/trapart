b85ba0f58e51a5438d22eaf7fc2883c15f81eb37
Frog Chen <satgo1546@yeah.net>
2024-10-21 18:08:44 +0800

    Remove code too specific

--- src/components/basic-controls/ToolTip.vue
+++ src/components/basic-controls/ToolTip.vue
@@ -12,23 +12,15 @@ const toolTip = ref<HTMLDivElement | null>(null)
 const toolTipVisible = ref(false)
 const toolTipStyles = useFloating(computed(() => ({
   getBoundingClientRect() {
-    let x = mouseX + 2
-    let y = mouseY + 16
-    // if (info.cursorShape === Qt.IBeamCursor) y -= 6;
-    if (cursorShape === 'text' || cursorShape === 'auto') y -= 6
-    // if (info.cursorShape === Qt.ArrowCursor && Qt.platform.os === "windows") y += 4;
-    if (cursorShape === 'default' && navigator.platform === 'Win32') y += 4
-    return new DOMRectReadOnly(x, y, 0, 0)
+    return new DOMRectReadOnly(mouseX + 2, mouseY, 0, 16)
   }
 })), toolTip, {
   placement: 'bottom-start',
   strategy: 'fixed',
-  // var screen = TkoolAPI.desktopAvailableGeometryAtPoint(Qt.point(x, y));
   middleware: [shift({ mainAxis: true, crossAxis: true })],
 }).floatingStyles
 let mouseX = 0
 let mouseY = 0
-let cursorShape = 'default'
 let mouseEntered = false
 let nextTime = NaN
 
@@ -50,9 +42,6 @@ function timer1() {
 function updateMouse(event: PointerEvent | MouseEvent) {
   mouseX = event.clientX
   mouseY = event.clientY
-  if (event.target instanceof Element) {
-    cursorShape = getComputedStyle(event.target).cursor
-  }
 }
 
 function onEntered(event: PointerEvent | MouseEvent) {
