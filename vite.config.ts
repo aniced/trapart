01768bdde638486ae43340a9ea9c194538fb4230
Frog Chen <satgo1546@yeah.net>
2025-02-11 11:15:31 +0800

    Fix Vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,8 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
-  // resolveSnapshotPath: (testPath, snapExtension) => testPath + snapExtension,
+  test: {
+    environment: 'node',
+    resolveSnapshotPath: (testPath, snapExtension) => testPath + snapExtension,
+  },
 })
